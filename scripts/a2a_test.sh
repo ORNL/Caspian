cc12a15c6d98ff0fb60ed3b399ae1975af526d72
Parker Mitchell <parker@ieee.org>
2020-05-11 15:40:08 -0400

    new test script for all-to-all networks

--- scripts/a2a_test.sh
+++ scripts/a2a_test.sh
@@ -9,22 +9,24 @@ tst() {
     a=`bin/all_to_all_bench sim $n 1 $t $s $dl | grep "$kw" - | cut -d ':' -f 2 | xargs`
     b=`bin/all_to_all_bench ucaspian $n 1 $t $s $dl | grep "$kw" - | cut -d ':' -f 2 | xargs`
     c=`bin/all_to_all_bench verilator $n 1 $t $s $dl | grep "$kw" - | cut -d ':' -f 2 | xargs`
-    v="Y"
+    vv="Y"
+    vf="Y"
 
     if ((a != c)); then
-        v="X"
-    elif ((a != b)); then
-        v="X"
+        vv="X"
+    fi
+    if ((a != b)); then
+        vf="X"
     fi
 
-    printf "%3d %3d | %5d %5d %5d %4s\n" $n $s $a $b $c $v
+    printf "%3d %3d | %5d %5d %5d %3s    %3s\n" $n $s $a $b $c $vf $vv
 }
 
-printf "%3s %3s | %5s %5s %5s %s\n" "N" "S" "Sim" "FPGA" "SVsim" "Correct?"
+printf "%3s %3s | %5s %5s %5s %s %s\n" "N" "S" "Sim" "FPGA" "SVsim" "Match?" "Match?"
 
-for i in {8..32}
+for i in {2..64}
 do
-    for j in {0..9}
+    for j in {0..99}
     do
         tst $i $j
     done
